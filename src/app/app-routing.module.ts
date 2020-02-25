ea529dd4e8ea6679be6f246f79d4e9eb6fddba54
Marc Bakker <webdev@marcbakker.com>
2020-02-25 12:47:38 +0100

    initial commit: setup routing, components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import ROUTES from "../constants/Routes";
+import { InvoicesComponent } from "./components/invoices/invoices.component";
+import { BillsComponent } from "./components/bills/bills.component";
+import { CompaniesComponent } from "./components/companies/companies.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { StatisticsComponent } from "./components/statistics/statistics.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: InvoicesComponent },
+  { path: ROUTES.INVOICES, component: InvoicesComponent },
+  { path: ROUTES.BILLS, component: BillsComponent },
+  { path: ROUTES.COMPANIES, component: CompaniesComponent },
+  { path: ROUTES.ADMIN, component: AdminComponent },
+  { path: ROUTES.STATISTICS, component: StatisticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { InvoicesComponent } from "./components/invoices/invoices.component";
+import { CompaniesComponent } from "./components/companies/companies.component";
+import { BillsComponent } from "./components/bills/bills.component";
+import { StatisticsComponent } from "./components/statistics/statistics.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { SignOutComponent } from "./components/sign-out/sign-out.component";
+import ROUTES from "../constants/Routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    InvoicesComponent,
+    CompaniesComponent,
+    BillsComponent,
+    StatisticsComponent,
+    AdminComponent,
+    SignOutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ROUTES],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
