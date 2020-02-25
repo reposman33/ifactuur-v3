90f33a186f5598f228d7139205a326212ea69c05
Marc Bakker <webdev@marcbakker.com>
2020-02-25 19:11:57 +0100

    Add AuthenticationService

---
+++ src/app/services/Authentication.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthenticationService {
+  userData$: Observable<firebase.User>;
+
+  constructor(private angularFireAuth: AngularFireAuth) {
+    this.userData$ = angularFireAuth.authState;
+  }
+
+  // AUTH API
+  SignUp(email: string, password: string) {
+    this.angularFireAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => console.log(res))
+      .catch(err => console.log("ERROR: ", err));
+  }
+
+  PasswordReset(email: string) {
+    this.angularFireAuth.auth
+      .sendPasswordResetEmail(email)
+      .then(res => console.log(res))
+      .catch(err => console.log("ERROR: ", err));
+  }
+
+  SignIn(email: string, password: string) {
+    this.angularFireAuth.auth
+      .signInWithEmailAndPassword(email, password)
+      .then(res => console.log(res))
+      .catch(err => console.log("ERROR: ", err));
+  }
+
+  SignOut() {
+    this.angularFireAuth.auth
+      .signOut()
+      .then(res => console.log(res))
+      .catch(err => console.log("ERROR: ", err));
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyD2V1xYoqOw6OYXwZ91xSFGeVbqFyRftYE",
+  authDomain: "aanvragen-a41a3.firebaseapp.com",
+  databaseURL: "https://aanvragen-a41a3.firebaseio.com",
+  projectId: "aanvragen-a41a3",
+  storageBucket: "aanvragen-a41a3.appspot.com",
+  messagingSenderId: "783966682970",
+  appId: "1:783966682970:web:959aea1a8a59bc95cfbd63"
 };
 
 /*
