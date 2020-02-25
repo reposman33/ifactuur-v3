6493e382943dda9b64d44adb4ada71743cc4b85b
Marc Bakker <webdev@marcbakker.com>
2020-02-25 19:12:41 +0100

    Add SignOut component

--- src/app/components/sign-out/sign-out.component.ts
+++ src/app/components/sign-out/sign-out.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "./../../services/Authentication.service";
 @Component({
-  selector: 'app-sign-out',
-  templateUrl: './sign-out.component.html',
-  styleUrls: ['./sign-out.component.scss']
+  selector: "app-sign-out",
+  templateUrl: "./sign-out.component.html",
+  styleUrls: ["./sign-out.component.scss"]
 })
 export class SignOutComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  handleOnSignOut() {
+    this.authService.SignOut();
   }
 
+  ngOnInit() {}
 }
