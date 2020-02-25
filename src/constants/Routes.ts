47b22e3ad049ab31cdbdd1144b3beaef80461fc7
Marc Bakker <webdev@marcbakker.com>
2020-02-25 13:36:36 +0100

    add Routes class with static classMembers

---
+++ src/constants/Routes.ts
@@ -0,0 +1,16 @@
+export default class ROUTES {
+  static readonly ADMIN = "admin";
+  static readonly BILLS = "bills";
+  static readonly BILL = "bill";
+  static readonly COMPANIES = "companies";
+  static readonly COMPANY = "company";
+  static readonly INVOICES = "invoices";
+  static readonly INVOICE = "invoice";
+  static readonly NAVIGATION = "navigation";
+  static readonly PASSWORD_CHANGE = "passwordchange";
+  static readonly PASSWORD_FORGET = "passwordforget";
+  static readonly SIGN_IN = "signin";
+  static readonly SIGN_OUT = "signout";
+  static readonly SIGN_UP = "signup";
+  static readonly STATISTICS = "statistics";
+}
