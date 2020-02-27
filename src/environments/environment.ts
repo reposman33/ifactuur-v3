1d29e09433fa775ffca8cba538a3dbbdfc732add
Marc Bakker <webdev@marcbakker.com>
2020-02-27 21:49:48 +0100

    environment: add Firebase config.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,16 @@
 
 export const environment = {
   production: false,
-  apiKey: "AIzaSyD2V1xYoqOw6OYXwZ91xSFGeVbqFyRftYE",
-  authDomain: "aanvragen-a41a3.firebaseapp.com",
-  databaseURL: "https://aanvragen-a41a3.firebaseio.com",
-  projectId: "aanvragen-a41a3",
-  storageBucket: "aanvragen-a41a3.appspot.com",
-  messagingSenderId: "783966682970",
-  appId: "1:783966682970:web:959aea1a8a59bc95cfbd63"
+  firebaseConfig: {
+    production: false,
+    apiKey: "AIzaSyD2V1xYoqOw6OYXwZ91xSFGeVbqFyRftYE",
+    authDomain: "aanvragen-a41a3.firebaseapp.com",
+    databaseURL: "https://aanvragen-a41a3.firebaseio.com",
+    projectId: "aanvragen-a41a3",
+    storageBucket: "aanvragen-a41a3.appspot.com",
+    messagingSenderId: "783966682970",
+    appId: "1:783966682970:web:959aea1a8a59bc95cfbd63"
+  }
 };
 
 /*
