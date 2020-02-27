f547112bd5f607c77ee3ed404c68700640baa404
Marc Bakker <webdev@marcbakker.com>
2020-02-27 17:30:29 +0100

    CSS: Assign container class to components, remove wrapper around <router-outlet>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,29 @@ import { BillsComponent } from "./components/bills/bills.component";
 import { CompaniesComponent } from "./components/companies/companies.component";
 import { AdminComponent } from "./components/admin/admin.component";
 import { StatisticsComponent } from "./components/statistics/statistics.component";
+import { AuthGuardService as AuthGuard } from "./services/auth-guard.service";
+import { SignInComponent } from "./components/sign-in/sign-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 const routes: Routes = [
-  { path: "", component: InvoicesComponent },
-  { path: ROUTES.INVOICES, component: InvoicesComponent },
+  {
+    path: ROUTES.INVOICES,
+    component: InvoicesComponent
+  },
   { path: ROUTES.BILLS, component: BillsComponent },
-  { path: ROUTES.COMPANIES, component: CompaniesComponent },
+  {
+    path: ROUTES.COMPANIES,
+    component: CompaniesComponent
+  },
   { path: ROUTES.ADMIN, component: AdminComponent },
-  { path: ROUTES.STATISTICS, component: StatisticsComponent }
+  {
+    path: ROUTES.STATISTICS,
+    component: StatisticsComponent
+  },
+  { path: ROUTES.SIGN_IN, component: SignInComponent },
+  { path: ROUTES.SIGN_UP, component: SignUpComponent },
+  { path: "", redirectTo: ROUTES.INVOICES, pathMatch: "full" },
+  { path: "**", redirectTo: ROUTES.INVOICES }
 ];
 
 @NgModule({
