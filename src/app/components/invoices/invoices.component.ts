e4ce285f301bf17b13bd763eed9486fdb863a27f
Marc Bakker <webdev@marcbakker.com>
2020-02-28 23:11:41 +0100

    Add Invoices component

--- src/app/components/invoices/invoices.component.ts
+++ src/app/components/invoices/invoices.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { I18nService } from "src/app/services/i18n.service";
+import { TAX_VALUES } from "../../../constants/misc";
 
 @Component({
-  selector: 'app-invoices',
-  templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.scss']
+  selector: "app-invoices",
+  templateUrl: "./invoices.component.html",
+  styleUrls: ["./invoices.component.scss"]
 })
 export class InvoicesComponent implements OnInit {
+  DECIMAL_SIGN;
+  totalBeforeTax;
+  tax;
+  FIELDNAMES;
+  taxFormatted;
+  totalBeforeTaxFormatted;
+  totalFormatted;
+  TAX_VALUES = TAX_VALUES;
 
-  constructor() { }
+  get;
 
-  ngOnInit() {
+  constructor() {
+    this.get = I18nService.get;
+    this.DECIMAL_SIGN = I18nService.getLocale() === "en" ? "." : ",";
+    this.totalBeforeTax = undefined;
+    this.tax = undefined;
+    this.FIELDNAMES = {
+      HOURLYRATEINT: "hourlyRateInt",
+      HOURLYRATEDEC: "hourlyRateDec",
+      HOURS: "hours",
+      TAX: "tax"
+    };
+    this.taxFormatted = undefined;
+    this.totalBeforeTaxFormatted = undefined;
+    this.totalFormatted = undefined;
   }
 
+  ngOnInit() {}
 }
