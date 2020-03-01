0d3f5f1d91c6c8b9a20fba95ae5ab88e6f682a93
Marc Bakker <webdev@marcbakker.com>
2020-03-01 20:04:00 +0100

    add dummy data

--- src/app/components/invoices/list/invoices.component.ts
+++ src/app/components/invoices/list/invoices.component.ts
@@ -19,6 +19,51 @@ export class InvoicesComponent implements OnInit {
 
   get;
 
+  rowData = [
+    {
+      id: "100",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    },
+    {
+      id: "101",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    },
+    {
+      id: "102",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    },
+    {
+      id: "103",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    },
+    {
+      id: "104",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    },
+    {
+      id: "105",
+      date: "17 februari 2020",
+      company: "Akzo",
+      sum: "9.855, -",
+      status: "open"
+    }
+  ];
+
   constructor() {
     this.get = I18nService.get;
     this.DECIMAL_SIGN = I18nService.getLocale() === "en" ? "." : ",";
